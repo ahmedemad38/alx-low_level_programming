df4bdfe219364d515257c2b70d1ea0cee495d04a
Ahmed Emad <ahmedemad0546@gmail.com>
2023-03-16 08:46:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Description: A C program that prints with put function
  *
- * Return: Alawys 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
