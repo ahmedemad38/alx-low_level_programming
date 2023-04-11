e909eb8749348a213b4d48f3150933c37032317a
Ahmed Emad <ahmedemad0546@gmail.com>
2023-04-11 08:14:15 -0700

    aa

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,17 +3,11 @@
 
 /**
 * create_array - create array of size size and assign char c
-
 * @size: size of array
-
 * @c: char to assign
-
 * Description: creat array of size size and assign char c
-
 * Return: pointer to array, NULL if fail
-
 *
-
 */
 
 char *create_array(unsigned int size, char c)
